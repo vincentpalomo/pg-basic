b307f4771c1751533a65ea05c670d616d5cd1d63
Vincent Palomo <vincentpalomo@yahoo.com>
2023-01-10 20:38:09 -0600

    made function to fetch a single query id

--- index.js
+++ index.js
@@ -21,7 +21,18 @@ app.get('/todos', async (req, res) => {
 
 // get todo
 
-app.get('/todo/:id');
+app.get('/todos/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', [
+      id,
+    ]);
+
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 // create todo
 
